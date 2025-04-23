b70e758d485ddf0afb0ecdc998224614e7aa1d48
miracline2 <francesca4518@gmail.com>
2025-04-23 22:55:27 +0530

    Skeleton of Mood Diaries Project

---
+++ project3-MoodDiaries/moodDiaries.js
@@ -0,0 +1,15 @@
+const card = document.getElementsByClassName('card')
+const inputElement = document.createElement('input')
+card.append(inputElement)
+
+
+function handleEmotions(button) {
+    const emotion = button.id;
+    console.log("You clicked:", emotion);
+    // You can add logic like:
+    if (emotion === 'happy') {
+    
+      
+     }
+  }
+  
